40a7dae89b033edc7dd55a6c30b1af23ebb6edf5
Ivan Krawchik <86794528+IvanKrawchik@users.noreply.github.com>
2021-09-17 12:51:52 -0300

    Add files via upload

---
+++ Diario/js/index_diario.js
@@ -0,0 +1,28 @@
+var slideIndex = 0;
+showSlides();
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlidesss");
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if(slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides,4000);
+}
+
+function irArriba(pxPantalla){
+    window.addEventListener('scroll', () => {
+        var scroll = document.documentElement.scrollTop;
+        var top = document.getElementById('top');
+        
+        if(scroll > pxPantalla){
+            top.style.right = 20 +'px';
+        }
+        else{
+            top.style.right = -100 +'px';
+        }
+    })
+}
+irArriba(50);
